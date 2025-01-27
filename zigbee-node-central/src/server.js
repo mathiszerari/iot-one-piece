da413d173dd61501c0b501f7d98777a215a42afe
Sebastien FLEURY <sebastien@digitalseeder.com>
2025-01-27 01:40:24 +0100

    add comments

--- zigbee-node-central/src/server.js
+++ zigbee-node-central/src/server.js
@@ -3,7 +3,13 @@ var xbee_api = require('xbee-api');
 var C = xbee_api.constants;
 require('dotenv').config()
 
+if (!process.env.SERIAL_PORT)
+  throw new Error('Missing SERIAL_PORT environment variable');
 
+if (!process.env.SERIAL_BAUDRATE)
+  throw new Error('Missing SERIAL_BAUDRATE environment variable');
+
+// Replace with your serial port and baud rate (9600 by default)
 const SERIAL_PORT = process.env.SERIAL_PORT;
 
 // Ensure to configure your XBEE Module in API MODE 2
@@ -15,14 +21,17 @@ let serialport = new SerialPort(SERIAL_PORT, {
   baudRate: parseInt(process.env.SERIAL_BAUDRATE) || 9600,
 }, function (err) {
   if (err) {
-    return console.log('Error: ', err.message)
+    return console.log('Creating SerialPort', err.message)
   }
 });
 
 serialport.pipe(xbeeAPI.parser);
 xbeeAPI.builder.pipe(serialport);
 
+const BROADCAST_ADDRESS = "FFFFFFFFFFFFFFFF";
 serialport.on("open", function () {
+
+  //Sample local command to ask local Xbee module the value of NODE IDENTIFIER
   var frame_obj = { // AT Request to be sent
     type: C.FRAME_TYPE.AT_COMMAND,
     command: "NI",
@@ -31,9 +40,10 @@ serialport.on("open", function () {
 
   xbeeAPI.builder.write(frame_obj);
 
+  //Sample remote command to ask all remote Xbee modules the value of their NODE IDENTIFIER
   frame_obj = { // AT Request to be sent
     type: C.FRAME_TYPE.REMOTE_AT_COMMAND_REQUEST,
-    destination64: "FFFFFFFFFFFFFFFF",
+    destination64: BROADCAST_ADDRESS,
     command: "NI",
     commandParameter: [],
   };
@@ -42,15 +52,14 @@ serialport.on("open", function () {
 });
 
 // All frames parsed by the XBee will be emitted here
-
-// storage.listSensors().then((sensors) => sensors.forEach((sensor) => console.log(sensor.data())))
-
 xbeeAPI.parser.on("data", function (frame) {
 
   //on new device is joined, register it
+  if (C.FRAME_TYPE.JOIN_NOTIFICATION_STATUS === frame.type) {
+    console.log("New device has joined network, you can register has new device available");
 
-  //on packet received, dispatch event
-  //let dataReceived = String.fromCharCode.apply(null, frame.data);
+  }
+
   if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
     console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
     let dataReceived = String.fromCharCode.apply(null, frame.data);
@@ -61,13 +70,13 @@ xbeeAPI.parser.on("data", function (frame) {
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
     // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
     console.log("NODE_IDENTIFICATION");
-    //storage.registerSensor(frame.remote64)
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
 
     console.log("ZIGBEE_IO_DATA_SAMPLE_RX")
+    console.log(frame)
+    console.log("Value of ADO can be retrieved with frame.analogSamples.AD0")
     console.log(frame.analogSamples.AD0)
-    //storage.registerSample(frame.remote64,frame.analogSamples.AD0 )
 
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
     console.log("REMOTE_COMMAND_RESPONSE")
