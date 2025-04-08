0119c55a6e3bf7f58f36f74941455d469750e060
Mathis Zerari <mathiszerari@icloud.com>
2025-04-08 11:53:27 +0200

    remove unused SVG icon and clean up CSS files

--- iot-app/src/pages/init.page.tsx
+++ iot-app/src/pages/init.page.tsx
@@ -1,34 +1,27 @@
-import React from 'react';
-import '../App.css'
+import React from "react";
+import "../App.css";
 
 const OnePiecePage: React.FC = () => {
     return (
-        <div className="bg-blue-500 text-white flex flex-col items-center justify-center min-h-screen">
-            <div className="text-center">
-                <h1 className="text-4xl md:text-6xl font-bold mb-4">Bienvenue sur IoT One Piece</h1>
-                <p className="text-lg md:text-2xl mb-8">Plongez dans l'aventure avec Luffy et son équipage !</p>
-                <img src="https://via.placeholder.com/150" alt="One Piece Logo" className="mb-8 rounded-full" />
-                <button className="bg-red-500 text-white font-bold py-4 px-8 rounded-full border-4 animated-border hover:bg-red-600 transition duration-300">
+        <div className="bg-gray-900 text-white min-h-screen">
+            <div className="text-center mt-8">
+                <img
+                    src="https://logo-marque.com/wp-content/uploads/2021/09/One-Piece-Logo.png"
+                    alt="One Piece Logo"
+                    className="w-1/2 mx-auto"
+                />
+                <h1 className="text-4xl md:text-6xl font-bold mb-4">
+                    Bienvenue sur IoT One Piece
+                </h1>
+                <p className="text-lg md:text-2xl mb-8">
+                    Plongez dans l'aventure avec Luffy et son équipage !
+                </p>
+            </div>
+            <div className="text-center mb-8">
+                <button className="bg-red-500 text-white font-bold py-4 px-8 rounded-full border-4 hover:bg-red-600 transition duration-300">
                     GO
                 </button>
             </div>
-            <style>{`
-                @keyframes borderAnimation {
-                    0% {
-                        border-color: #ff6f61;
-                    }
-                    50% {
-                        border-color: #ffcc5c;
-                    }
-                    100% {
-                        border-color: #ff6f61;
-                    }
-                }
-
-                .animated-border {
-                    animation: borderAnimation 2s infinite;
-                }
-            `}</style>
         </div>
     );
 };
