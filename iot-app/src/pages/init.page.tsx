54d1bfd8d257d8a95c0b11e377fe2b69f70863c6
Mathis Zerari <mathiszerari@icloud.com>
2025-04-08 14:56:14 +0200

    refactor OnePiecePage layout and styling for improved UI

--- iot-app/src/pages/init.page.tsx
+++ iot-app/src/pages/init.page.tsx
@@ -4,12 +4,14 @@ import "../App.css";
 const OnePiecePage: React.FC = () => {
     return (
         <div className="bg-gray-900 text-white min-h-screen">
-            <div className="text-center mt-8">
+        <div className="text-center">
+          <div className="mt-4">
                 <img
                     src="https://logo-marque.com/wp-content/uploads/2021/09/One-Piece-Logo.png"
                     alt="One Piece Logo"
                     className="w-1/2 mx-auto"
                 />
+          </div>
                 <h1 className="text-4xl md:text-6xl font-bold mb-4">
                     Bienvenue sur IoT One Piece
                 </h1>
@@ -17,8 +19,8 @@ const OnePiecePage: React.FC = () => {
                     Plongez dans l'aventure avec Luffy et son équipage !
                 </p>
             </div>
-            <div className="text-center mb-8">
-                <button className="bg-red-500 text-white font-bold py-4 px-8 rounded-full border-4 hover:bg-red-600 transition duration-300">
+            <div className="text-center pt-8">
+                <button className="bg-gray-800 text-white text-2xl font-bold p-8 rounded-full border-4 hover:bg-red-600 transition duration-300">
                     GO
                 </button>
             </div>
