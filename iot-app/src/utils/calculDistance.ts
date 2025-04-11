4e278488badbe50948092087d4f5d3a0b8098ee6
NoeHLM <noeheylemadec@gmail.com>
2025-04-11 09:43:51 +0200

    finish steps

--- iot-app/src/utils/calculDistance.ts
+++ iot-app/src/utils/calculDistance.ts
@@ -1,3 +1,4 @@
+import { SensorType } from "../models/sensors.enum";
 import { sendToTopic } from "./mqttFunctions";
 
 interface CalculationResult {
@@ -5,6 +6,8 @@ interface CalculationResult {
   passed: boolean;
   nextstep?: number;
 }
+
+let lastValue = 0
 
 export const calculateDistance = (value: number, step: number): CalculationResult => {
   if (step === 1) {
@@ -17,27 +20,32 @@ export const calculateDistance = (value: number, step: number): CalculationResul
 
   function lightCalcul(): CalculationResult {
     let max = 100;
-    let mid = 50;
-    let low = 0;
+    let low = 70;
+
+      console.log(value)
 
     if (value >= max) {
       return { message: "Ah non tu n'y es pas", passed: false };
-    } else if (value < max && value >= mid) {
+    } else if (value < max && value >= low) {
       return { message: "Ça chauffe, continue comme ça !", passed: false };
     } else if (value < low) {
+      // maybe add delay
       sendToTopic("box/step", `step-2`);
       return { message: "Félicitation, c'est réussi !", passed: true, nextstep: 2 };
 
     }
-    return { message: "Analyse en cours", passed: false };
+    return { message: "", passed: false };
   }
 
   function pressureCalcul(): CalculationResult {
-    let limit = 30
+    let limit = 150
+
+      console.log(value)
 
     if (value <= limit) {
       return { message: "Ah non tu n'y es pas", passed: false };
     } else if (value > limit) {
+      // maybe add delay
       sendToTopic("box/step", `step-3`);
       return { message: "Félicitation, c'est réussi !", passed: true, nextstep: 3 };
 
@@ -45,18 +53,25 @@ export const calculateDistance = (value: number, step: number): CalculationResul
     return { message: "Analyse en cours", passed: false };
   }
 
-  function soundCalcul(): CalculationResult {
-    let limit = 200
+    function soundCalcul(): CalculationResult {
+        console.log(value)
 
-    if (value <= limit) {
-      return { message: "Ah non tu n'y es pas", passed: false };
-    } else if (value > limit) {
-      sendToTopic("box/step", `step-4`);
-      return { message: "Félicitation, c'est réussi !", passed: true, nextstep: 4 };
+        const result: CalculationResult =
+            value < lastValue
+                ? { message: "Félicitation, c'est réussi !", passed: true, nextstep: 4 }
+                : { message: "Ah non tu n'y es pas", passed: false };
 
+        if (result.passed) {
+            sendToTopic("box/step", "step-4");
+        }
+
+        if (Number.isInteger(value)) {
+            lastValue = value;
+        }
+
+        return result;
     }
-    return { message: "Analyse en cours", passed: false };
-  }
 
-  return { message: "Erreur", passed: false };
+
+    return { message: "Erreur", passed: false };
 }
